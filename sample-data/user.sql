cbe53ef7a2107e164d369c88273d06e2ee0c7cca
sebawita <sebawita@gmail.com>
2019-05-20 18:40:15 +0100

    added sample data

---
+++ sample-data/user.sql
@@ -0,0 +1,34 @@
+create table user (
+	id INT,
+	username VARCHAR(50),
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	avatar VARCHAR(50),
+	password VARCHAR(50)
+);
+insert into user (id, username, first_name, last_name, email, avatar, password) values (1, 'rhenkmann0', 'Reggi', 'Henkmann', 'rhenkmann0@vkontakte.ru', 'https://robohash.org/earumexmollitia.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (2, 'kfaichnie1', 'Kitty', 'Faichnie', 'kfaichnie1@zdnet.com', 'https://robohash.org/accusamusestnulla.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (3, 'rbezarra2', 'Rance', 'Bezarra', 'rbezarra2@ted.com', 'https://robohash.org/perspiciatiscorporisprovident.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (4, 'lsurphliss3', 'Lavina', 'Surphliss', 'lsurphliss3@vimeo.com', 'https://robohash.org/quiadipsa.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (5, 'tpien4', 'Talia', 'Pien', 'tpien4@examiner.com', 'https://robohash.org/ineiusperferendis.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (6, 'groyl5', 'Gloria', 'Royl', 'groyl5@bloglovin.com', 'https://robohash.org/fugitfacilisomnis.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (7, 'cscotts6', 'Cristal', 'Scotts', 'cscotts6@state.tx.us', 'https://robohash.org/adsuntaliquam.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (8, 'rfandrich7', 'Rodrique', 'Fandrich', 'rfandrich7@dedecms.com', 'https://robohash.org/corporisrerumvoluptas.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (9, 'rlecount8', 'Riki', 'Le Count', 'rlecount8@wix.com', 'https://robohash.org/utatest.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (10, 'kridgers9', 'Kippy', 'Ridgers', 'kridgers9@icio.us', 'https://robohash.org/sequiullamconsequuntur.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (11, 'cdyasa', 'Chad', 'Dyas', 'cdyasa@time.com', 'https://robohash.org/suscipitofficiaminus.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (12, 'hklinkb', 'Heindrick', 'Klink', 'hklinkb@jimdo.com', 'https://robohash.org/autveniamsaepe.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (13, 'cscholigc', 'Carmine', 'Scholig', 'cscholigc@huffingtonpost.com', 'https://robohash.org/quisolutapraesentium.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (14, 'yhuntressd', 'Yorgos', 'Huntress', 'yhuntressd@auda.org.au', 'https://robohash.org/quoquiexplicabo.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (15, 'dmaycocke', 'Darill', 'Maycock', 'dmaycocke@sitemeter.com', 'https://robohash.org/etsitassumenda.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (16, 'kgateleyf', 'Kelila', 'Gateley', 'kgateleyf@sciencedirect.com', 'https://robohash.org/voluptatessaepepariatur.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (17, 'dsporrijg', 'Dalton', 'Sporrij', 'dsporrijg@zimbio.com', 'https://robohash.org/providentvelconsequatur.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (18, 'tbickerstasseh', 'Torrance', 'Bickerstasse', 'tbickerstasseh@sun.com', 'https://robohash.org/velquamomnis.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (19, 'ecancutti', 'Emilee', 'Cancutt', 'ecancutti@delicious.com', 'https://robohash.org/eosaliquamunde.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (20, 'tloguej', 'Thaddeus', 'Logue', 'tloguej@businesswire.com', 'https://robohash.org/veritatisdoloremquererum.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (21, 'alaprek', 'Aurel', 'Lapre', 'alaprek@hao123.com', 'https://robohash.org/illumetsit.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (22, 'hhenriel', 'Hi', 'Henrie', 'hhenriel@linkedin.com', 'https://robohash.org/iurenonet.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (23, 'rbroginim', 'Rowan', 'Brogini', 'rbroginim@alibaba.com', 'https://robohash.org/veritatisutet.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (24, 'jlevenen', 'Joyce', 'Levene', 'jlevenen@indiatimes.com', 'https://robohash.org/namrerumdolor.png?size=200x200&set=set1', 'pass');
+insert into user (id, username, first_name, last_name, email, avatar, password) values (25, 'pcripino', 'Phillie', 'Cripin', 'pcripino@woothemes.com', 'https://robohash.org/sintetrepellat.png?size=200x200&set=set1', 'pass');
